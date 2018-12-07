788ea155d371883a7b44b837a9f74ee7f433cb67
Ubuntu <ubuntu@ip-172-31-85-158.ec2.internal>
2018-12-07 21:23:45 +0000

    commiting rappture tool!

--- HW40/root/king_model.py
+++ HW40/root/king_model.py
@@ -24,55 +24,63 @@ def king(y,x):
     return dydx
 
 def main():
-
     # Get arguments
     io = Rappture.library(sys.argv[1])
-    a = float(io.get('input.number(sys.argv[1]).current'))
-    b = float(io.get('input.number(sys.argv[2]).current'))
-    x = np.linspace(0, a, 100)   # Use rappture to input second number
-    y_init=[b, 0]      # Use rappture to input first number
-    #formula = io.get('input.string(formula).current')
-
-    sol=odeint(king, y_init, x)
     
+    x = 0.0001
+    dx = 0.0001
+    psi0 = float(io.get('input.number(psi0).current'))
+    y_init =[psi0,0]
+
+    r = []
+    psi = []
+    dens = []
+    yy = 1
+    while yy >= 1.e-8:
+        sol=odeint(king, y_init, [0,x])
+        yy = sol[1,0]
+        r.append(x)
+        psi.append(yy)
+        dens.append(rho(yy))
+        dx = min(2. * dx, 0.02 * sol[1,0] / abs(sol[1,1]))
+        x += dx
+
+        v_max = np.sqrt(2.) * np.sqrt(psi)
+
+    
+    for i in range(len(r)):
+        io.put(
+                'output.curve(result1).component.xy',
+                '%g %g\n'%(r[i],dens[i]), append=1
+                )
+        
     io.put('output.curve(result1).about.label','density vs. r',append=0)
     io.put('output.curve(result1).yaxis.label','density')
     io.put('output.curve(result1).xaxis.label','radius')
-
-    for i in range(5):
+        
+        
+    for i in range(len(r)):
         io.put(
-               'output.curve(result1).component.xy',
-               '%g %g\n' % (x[i],sol[i][0]), append=1
+                'output.curve(result2).component.xy',
+                '%g %g\n'%(r[i],v_max[i]), append=1
               )
+        
+    io.put('output.curve(result2).about.label','v_max vs. r',append=0)
+    io.put('output.curve(result2).yaxis.label','v_max')
+    io.put('output.curve(result2).xaxis.label','radius')
+        
+    for i in range(len(r)):
+        io.put(
+                'output.curve(result3).component.xy',
+                '%g %g\n'%(r[i],psi[i]), append=1
+        )
 
+    io.put('output.curve(result3).about.label','psi vs. r',append=0)
+    io.put('output.curve(result3).yaxis.label','psi')
+    io.put('output.curve(result3).xaxis.label','radius')
 
-    # Get base string
-
-    my_str_base = 'int_0^root (' + king(y,x) + ') dydx - ' + str(y)
-
-    # Get compute root of \int_0^x f(x') dx' - a
-
-    #root = optimize.brentq(
-    #           calc, xmin, xmax, args=(a, formula
-    #        )
-
-    #plt.plot(x, sol[:,0])
-    #plt.show()
-    #my_str = 'Root of f(x) in the range ' + str(xmin) + ' to ' + \
-    #         str(xmax) + ' is ' + str(root)
-    #io.put('output.string(result1).about.label', 'Root')
-    #io.put('output.string(result1).current', my_str)
 
     Rappture.result(io)
-
-
-    # Check
-
-    #check = calc(root, a, formula)
-
-    #my_str2 = '\nCheck: ' + my_str_base + ' = ' + str(check[0])
-
-    #print(my_str2 + '\n')
 
 if __name__ == "__main__":
     main()
--- HW40/root/plot_lambda.py
+++ HW40/root/plot_lambda.py
@@ -24,20 +24,45 @@ def king(y,x):
 
 def main():
 
-    x = np.linspace(0, float(sys.argv[1]), 100)   # Use rappture to input second number
-    y_init=[float(sys.argv[2]), 0]      # Use rappture to input first number
+    x = 0.0001
+    dx = 0.0001
+    y_init=[float(sys.argv[1]), 0]      # Use rappture to input first number
+    yy = y_init[0]
+ 
+    r = []
+    psi = []
+    dens = []
+    while yy >= 1.e-8:
+        sol=odeint(king, y_init, [0,x])
+        yy = sol[1,0]
+        r.append(x * np.sqrt(float(sys.argv[1])))
+        psi.append(yy)
+        dens.append(rho(yy))
+        dx = min(2. * dx, 0.02 * sol[1,0] / abs(sol[1,1]))
+        x += dx
 
-    sol=odeint(king, y_init, x)
+    v_max = np.sqrt(2.) * np.sqrt(psi)
 
-    plt.plot(x, sol[:,0])
+    if sys.argv[2] == 'v_max':
+        plt.plot(r, v_max)
+        plt.ylabel('${\\rm v_{max}}$')
+    elif sys.argv[2] == 'rho':
+        plt.plot(r, dens/dens[0])
+        plt.ylabel('${\\rm \\rho / \\rho_0}$')
+    else:
+        plt.plot(r, psi)
+        plt.ylabel('${\\rm \\Psi}$')
+
+    #plt.xscale('log')
+    #plt.yscale('log')
+
+    #plt.xlim([0.1,200])
+    #plt.ylim([1.e-6,10])
+
+    plt.xlabel('${\\rm r / r_0}$')
 
     plt.show()
 
 if __name__ == "__main__":
     main()
 
-#plt.plot(rho(y),r,label='rho(y) vs. r')
-#plt.xlabel('radius')
-#plt.ylabel('rho(y)')
-#plt.legend()
-#plt.show()
